6eccd6aa6c0d6b3940c1a3087e82912a1334ab65
Ahmed Gabr <Ahmed.Gabr@appyinnovator.com>
2024-08-28 21:41:27 +0300

    first commit

---
+++ js/staticData.js
@@ -0,0 +1,14 @@
+// type of the secondary identification
+
+const typeOfSecondaryIdentification = [
+    "رقم التعريف الضريبي - Tax Identification Number",
+    "رقم جواز السفر - Passport ID",
+    "اخري - Other",
+    "السجل التجاني -  CR - Commercial Registration Number",
+    "ترخيص وزاره الشوؤن البلدية و القروية - MOMRAH License",
+    "ترخيص وزارة الموارد البشرية و التنمية الاجتماعية - MHRSD License",
+    "رقم ٧٠٠ - Number 700",
+    "ترخيص الهيئة العامة للاستثمار - MISA License",
+    "الهوية الوطنية - National ID",
+    "معرف دول مجلس التعاون الخليجي - GCC ID"
+]
